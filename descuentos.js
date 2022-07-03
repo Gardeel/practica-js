e660767bc3f6d58a19d3d70814e93e1a1d2eb5fc
Cristian Calderon <cdcalderonrojas@gmail.com>
2022-07-03 16:51:29 +0200

    actualizando archivos

---
+++ descuentos.js
@@ -0,0 +1,32 @@
+// const precioOriginal = 100;
+// const descuento = 15;
+
+function calcularPrecioConDescuento(precio, descuento){
+    const porcentajePrecioConDescuento = 100 - descuento;
+    const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
+    
+    return precioConDescuento;
+}
+
+
+function buttonPriceDiscount(){
+    const inputPrice = document.getElementById("inputPrice");
+    const priceValue = inputPrice.value;
+
+
+    const inputDiscount = document.getElementById("inputDiscount");
+    const discountValue = inputDiscount.value;
+
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
+
+    const resultadoP = document.getElementById("ResultP"); 
+    resultadoP.innerText = `El precio con descuento son$ ${precioConDescuento}`;
+}
+
+
+// console.log({
+//     precioOriginal,
+//     descuento,
+//     porcentajePrecioConDescuento,
+//     precioConDescuento
+// });
