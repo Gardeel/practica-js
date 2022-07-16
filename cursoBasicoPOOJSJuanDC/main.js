3dce4bb4d20b17de895776e1177860cd0aee4e5a
Cristian Calderon <cdcalderonrojas@gmail.com>
2022-07-16 14:11:53 +0200

    Clases en JavaScript

--- cursoBasicoPOOJSJuanDC/main.js
+++ cursoBasicoPOOJSJuanDC/main.js
@@ -1,19 +1,46 @@
 // Objetos literales y prototipos en JavaScript
 
+// Objeto Literal
 const cristian = {
     name: "Cris",
     age: 29,
-    cursosAprovados: [
+    cursosAprobados: [
         "Curso Definitivo de HTML y CSS",
         "Curso Practico de HTML y CSS",
 
     ],
 
     // creamos un metodo dentro de un objeto literal
-
     aprobarCurso(nuevoCurso){
-        this.cursosAprovados.push(nuevoCurso)
+        this.cursosAprobados.push(nuevoCurso)
     }
 };
 
 
+// OBJETOS 
+function Student (name, age, cursosAprobados){
+    this.name = name;
+    this.age = age; 
+    this.cursosAprobados = cursosAprobados;
+    
+    // crear una funcion interna
+    // this.aprobarCurso = function (nuevoCurso){
+    //     this.cursosAprobados.push(nuevoCurso);
+    // }    
+}
+
+// llamar por fuera una funcion 
+
+Student.prototype.aprobarCurso = function (nuevoCurso){
+        this.cursosAprobados.push(nuevoCurso);
+}
+
+
+const juanito = new Student (
+    "Juan Perez",
+    25,
+    [
+        "Curso a la Introduccion a los VideoJuegos",
+        "Curso de creacion de Personajes",
+    ],
+);
