638995faa2e95644b256ee26dddc9ca6cbbb047f
Cristian Calderon <cdcalderonrojas@gmail.com>
2022-07-18 00:19:00 +0200

    Abstraccion en JavaScript 9

--- cursoBasicoPOOJSJuanDC/ventajas.js
+++ cursoBasicoPOOJSJuanDC/ventajas.js
@@ -1,3 +1,66 @@
+// Clase de un curso
+class Courses {
+    constructor({
+    name,
+    classes =[],
+    }){
+        this.name = name;
+        this.classes = classes;
+    }
+}
+
+const cursoProgBasica = new Courses ({
+    name: "Curso Gratis de Programacion Basica", 
+});
+
+const cursoDefinitivoHTML = new Courses ({
+    name: "Curso Definitivo de HTML y CSS", 
+});
+
+const cursoPracticoHTML = new Courses ({
+    name: "Curso Practico de HTML y CSS", 
+});
+
+// Clases de los cursos
+class LearningPath {
+    constructor({
+        name,
+        courses = []
+    }){
+        this.name = name;
+        this.courses = courses;
+    }
+}
+
+
+const escuelaWeb = new LearningPath ({
+    name: "Escuela de Desarrollo Web",
+    courses: [
+        cursoProgBasica,
+        cursoDefinitivoHTML,
+        cursoPracticoHTML,
+    ]
+});
+
+const escuelaData = new LearningPath ({
+    name: "Escuela de Data Science",
+    courses: [
+        cursoProgBasica,
+        "Curso DataBusiness",
+        "Curso Dataviz",
+    ]
+});
+
+
+const escuelaVgs = new LearningPath ({
+    name: "Escuela de Videojuegos",
+    courses: [
+        cursoProgBasica,
+        "Curso Unity",
+        "Curso Unreal",
+    ]
+});
+
 class Student {
     constructor({
         name,
@@ -28,5 +91,19 @@ const cris = new Student({
     username: "Gardel",
     email: "cdcalderonrojass@gmail.com",
     twitter: "@gardel",
+    learningPaths: [
+        escuelaWeb,
+        escuelaVgs,
+    ]
+});
 
+const miguel = new Student({
+    name: "Miguelito",
+    username: "miguelitofeliz",
+    email: "miguelito@juanito.com",
+    instagram: "@miguelito_feliz",
+    learningPaths: [
+        escuelaWeb,
+        escuelaData,
+    ]
 });
